300221cde7e1d6895c8ccc6c2362479cd3752e0d
Brendan Lambrecht <blambrecht04@gmail.com>
2025-07-19 11:00:37 -0500

    Update phishML.py

--- phishML.py
+++ phishML.py
@@ -4,17 +4,24 @@ import numpy as np
 # Machine learning model
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-# Visualization
+
+# Visualization | For Data Analysis and Understanding
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.tree import export_graphviz
-
-# Reading and Splitting the Dataset
-df = pd.read_csv('.../dataset.csv')
-dot_file = '.../tree.dot'
-confusion_matrix_file = '.../confusion_matrix.png'
-
-# Printing the Results of the Read Operation
-print(df.head())
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+phiusiil_phishing_url_website = fetch_ucirepo(id=967) 
+  
+# data (as pandas dataframes) 
+X = phiusiil_phishing_url_website.data.features 
+y = phiusiil_phishing_url_website.data.targets 
+  
+# metadata 
+print(phiusiil_phishing_url_website.metadata) 
+  
+# variable information 
+print(phiusiil_phishing_url_website.variables) 
